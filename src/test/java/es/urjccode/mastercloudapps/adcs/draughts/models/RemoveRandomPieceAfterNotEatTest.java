1fa68eb08d35a14cbf111d8bb9e0a0d0522cd967
Carlos Castro García <alu0100819847@ull.edu.es>
2020-04-25 18:12:26 +0200

    Solving some error en test values

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/RemoveRandomPieceAfterNotEatTest.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/RemoveRandomPieceAfterNotEatTest.java
@@ -4,36 +4,81 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class RemoveRandomPieceAfterNotEatTest {
+public class RemoveRandomPieceAfterNotEatTest extends GameTest{
 
     @Test
-    public void testCountCanJumpPieces(){
-        Game game = new GameBuilder().rows(
+    public void testRemoveAfterNotEatWhite(){
+        this.setGame(Color.WHITE,
             "        ",
             "        ",
-            "   n    ",
+            "   n   n",
+            "  b   n ",
+            "     b  ",
+            "b       ",
+            "        ",
+            "        ");
+        this.setExpectedGame(Color.BLACK,
+            "        ",
+            "        ",
+            "   n   n",
+            "      n ",
+            "     b  ",
+            "b       ",
+            "        ",
+            "        ");
+        this.game.move(new Coordinate(3,2), new Coordinate(2,1));
+        assertEquals(this.expectedGame, this.game);
+    }
+
+    @Test
+    public void testRemoveAfterNotEatBlack(){
+        this.setGame(Color.BLACK,
+            "        ",
+            "        ",
+            "   n   n",
             "  b b   ",
             "     b  ",
             "b       ",
             "        ",
-            "        ").build();
-        assertEquals(game.getCanJumpPieces().size(), 2);
-    }
-
-    @Test
-    public void testRemoveAfterNotEat(){
-        Game game = new GameBuilder().rows(
+            "        ");
+        this.setExpectedGame(Color.WHITE,
             "        ",
             "        ",
-            "   n   n",
+            "        ",
             "  b b n ",
             "     b  ",
             "b       ",
             "        ",
-            "        ").build();
-
-        game.move(new Coordinate(4,2), new Coordinate(3,1));
-        assertEquals(game.getCanJumpPieces().size(), 1);
+            "        ");
+        this.game.move(new Coordinate(2,7), new Coordinate(3,6));
+        assertEquals(this.expectedGame, this.game);
     }
+
+    @Test
+    public void testRemoveAfterNotEatWithDraught(){
+        this.setGame(Color.BLACK,
+            "        ",
+            "        ",
+            "       n",
+            "  b b   ",
+            "   N b  ",
+            "b       ",
+            "        ",
+            "        ");
+        this.setExpectedGame(Color.WHITE,
+            "        ",
+            "        ",
+            "        ",
+            "  b b n ",
+            "     b  ",
+            "b       ",
+            "        ",
+            "        ");
+        this.game.move(new Coordinate(2,7), new Coordinate(3,6));
+        assertEquals(this.expectedGame, this.game);
+
+    }
+
+
 
 }
